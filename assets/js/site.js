9004a9bd07362790a0179a556f4da8a0ccb65a3b
asudeozturk <aozturk2@hawk.iit.edu>
2022-02-24 01:03:26 -0600

    Hide navigation and summary for small screen

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,35 @@
 'use strict';
+var navigationBar = document.querySelector('#checkout-nav');
+var summary = document.querySelector('#summary');
+
+navigationBar.setAttribute('disabled', 'disabled');
+navigationBar.setAttribute('aria-hidden', 'true');
+summary.setAttribute('disabled', 'disabled');
+summary.setAttribute('aria-hidden', 'true');
+
+if(window.innerWidth >= 400) { //show checkout-nav at minimum 400px
+  navigationBar.removeAttribute('disabled');
+  navigationBar.setAttribute('aria-hidden', 'false');
+}
+if(window.innerWidth >= 800) { //show summary at minimum 800px
+  navigationBar.removeAttribute('disabled');
+  navigationBar.setAttribute('aria-hidden', 'false');
+}
+
+window.addEventListener('resize', function(event) {
+  if(window.innerWidth >= 400) {
+    navigationBar.removeAttribute('disabled');
+    navigationBar.setAttribute('aria-hidden', 'false');
+  } else {
+    navigationBar.setAttribute('disabled', 'disabled');
+    navigationBar.setAttribute('aria-hidden', 'true');
+  }
+
+  if(window.innerWidth >= 800) {
+    summary.removeAttribute('disabled');
+    summary.setAttribute('aria-hidden', 'false');
+  } else {
+    summary.setAttribute('disabled', 'disabled');
+    summary.setAttribute('aria-hidden', 'true');
+  }
+});
